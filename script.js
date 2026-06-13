97f222877ad7eac2072bce0a80baf1f9d20f1cc1
anrars <anreed760@gmail.com>
2026-06-13 12:57:17 -0400

    Added mission sections

---
+++ script.js
@@ -0,0 +1,19 @@
+function showMessage() {
+
+    const message = `
+    Final Transmission
+
+    I started this project because Destiny means a lot to you.
+
+    I wanted to create something from your world,
+    even though I am still learning about it.
+
+    Thank you for all the conversations,
+    the laughs,
+    and all the time we've spent together.
+
+    Eyes up, Guardian.
+    `;
+
+    document.getElementById("secret-message").innerText = message;
+}
